276339081a0c20592bb9294829391c90f3f8ba22
vanessa <escavanessa@gmail.com>
2024-01-27 21:16:26 -0500

    base added for gp

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Little-Lemon/',
   plugins: [react()],
   test: {
     globals: true,
