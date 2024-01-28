7d943ff27f975892e787582b81dd0eaafca17fc9
galford-tillster1 <galford@tillster.com>
2024-01-27 21:50:11 -0800

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
+    "./*.html",
     "./components/**/*.{js,jsx,}",
-    "./src/**/*.{js,jsx,css}"
+    "./src/**/*.{js,jsx,css}",
+    "./assets/**/*.{js,jsx,css}",
   ],
   theme: {
     screens: {
